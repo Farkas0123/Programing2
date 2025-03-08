900105db5085aeae014a11c1662cee888b69493a
Farkas0123 <89979780+Farkas0123@users.noreply.github.com>
2025-03-08 18:01:02 +0100

    Add files via upload

--- Seminar03.py
+++ Seminar03.py
@@ -1,3 +1,6 @@
+import time
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -62,10 +65,6 @@ class LinkedList:
         while current_node:
             print(current_node.data)
             current_node = current_node.next
-        
-        
-        
-        
 
 l = LinkedList()
 l.append('apple')
@@ -77,3 +76,129 @@ l.show_items()
 l.find('apple')
 
 
+#Exercise 2
+
+class ListQueue:
+    def __init__(self):
+        self.head = None        
+
+    def enqueue(self, item):
+        item = Node(item)
+        if self.head == None:
+            self.head = item
+            return
+        current = self.head
+        while current.next != None:
+            current = current.next
+        current.next = item
+        return
+
+    def pop(self, index):
+        result = self.head
+        self.head = self.head.next 
+        return result       
+
+    def peek(self):
+        print(self.head.data)
+
+    def size(self):
+        len = 0
+        current = self.head
+        while current:
+            current = current.next
+            len += 1
+        return len
+    
+l = ListQueue()
+l.enqueue(1)
+l.enqueue(1814)
+l.enqueue(123)
+l.enqueue(156)
+l.enqueue(13)
+l.enqueue(10)
+print(l.size())
+print(l.pop(0).data)
+print(l.size())
+l.peek()
+print(l.size())
+
+#exercise 2b
+def measure_time(queue_class, db = 100000):
+    queue = queue_class
+    start = time.time()
+    for i in range(100000):
+        queue.enqueue(i)
+    for i in range(100000):
+        queue.pop(0)
+    
+    
+    end = time.time()
+    return end-start
+
+#times = measure_time(ListQueue())
+#print(f'The time: {times}')
+
+#webhistory type shit
+class BrowserHistory:
+    def __init__(self):
+        self.head = None
+
+    def visit(self, url):
+        #Push a new URL onto the stack when a page is visited
+        url = Node(url)
+        if self.head == None:
+            self.head = url
+            return
+        current = self.head
+        self.head = url
+        self.head.next = current
+        return
+
+    def go_back(self):
+        #Pop the last URL from the stack to go back to the previous page
+        if self.head == None:
+            print('please search before looking at the search history')
+        result = self.head
+        self.head = self.head.next
+        return result.data
+        
+    def current_page(self):
+        if self.head == None:
+            print('Empyt history')
+            return    
+        return self.head.data
+
+    def show_history(self):
+        # Display the entire browsing history
+        result= ''
+        if self.head == None:
+            return result
+        current = self.head
+        while current.next != None:
+            result += str(current.data)+' '
+            current = current.next
+        result += current.data
+        return result
+#test
+# Create a new BrowserHistory object
+browser_history = BrowserHistory()
+
+# Visiting pages
+browser_history.visit("page1.com")
+browser_history.visit("page2.com")
+browser_history.visit("page3.com")
+browser_history.visit("page4.com")
+browser_history.visit("page5.com")
+browser_history.visit("page6.com")
+
+# Checking the current page
+print("Current page:", browser_history.current_page())
+
+# Going back
+print("Going back to:", browser_history.go_back())
+
+# Current page after going back
+print("Current page after going back:", browser_history.current_page())
+
+# Show the entire browsing history
+print("Browser history:", browser_history.show_history())            
